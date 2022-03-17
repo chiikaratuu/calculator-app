d0be283dac7c63f53644edffe360997558039b2c
chiikaratuu <chiikaratuu0901@students.amikom.ac.id>
2022-03-17 23:12:25 +0700

    inisialisasi project

--- calculator_app/calculator_app/Properties/AssemblyInfo.cs
+++ calculator_app/calculator_app/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication2")]
+[assembly: AssemblyTitle("calculator_app")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication2")]
+[assembly: AssemblyProduct("calculator_app")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0ed598a9-8347-483b-959e-17e7ea415229")]
+[assembly: Guid("7d127527-bb6a-4747-8e78-a59c222ad5e4")]
 
 // Version information for an assembly consists of the following four values:
 //
